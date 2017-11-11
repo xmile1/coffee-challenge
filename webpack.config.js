77159e891359a6d4cf64ace76afdb455c55c87b7
Uyiosa Enabulele <hiskonxeptz@gmail.com>
2017-11-11 12:01:24 +0100

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack')
+var path = require('path')
+module.exports = {
+  entry: path.resolve(__dirname, 'src'),
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve:{
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules', path.resolve(__dirname, 'src')]},
+  module: {
+     loaders: [
+         { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: 'babel-loader',
+        query: {
+          presets: ['react']
+        } }
+     ],
+   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+}
